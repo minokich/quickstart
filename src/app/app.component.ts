8387326ff38d7790a113ff3fc0944a8d1b536340
Minokich <mino0525dev@gmail.com>
2019-10-31 16:06:27 +0900

    @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Book } from "./books";
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: "my-app",
+  template: `
+    <div>
+      <span *ngFor="let b of books">
+        [<a href="#" (click)="onclick(b)">{{ b.title }}</a
+        >]
+      </span>
+    </div>
+    <hr />
+    <detail-book [item]="selected"></detail-book>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  selected: Book;
+  books: Book[] = [
+    {
+      isbn: "978-4-7973-9523-5",
+      title: "１分で話せ",
+      price: 1400,
+      publisher: "SBクリエイティブ"
+    },
+    {
+      isbn: "978-4-7741-9706-7",
+      title: "はじめてのフロントエンド開発",
+      price: 2600,
+      publisher: "技術評論社"
+    },
+    {
+      isbn: "978-4-7775-1765-7",
+      title: "Vue風景CGテクニックガイド",
+      price: 3000,
+      publisher: "工学社"
+    },
+    {
+      isbn: "978-4-0436-3603-7",
+      title: "アラビアの夜の種族",
+      price: 2000,
+      publisher: "角川書店"
+    }
+  ];
+
+  onclick(book: Book) {
+    this.selected = book;
+  }
+}
---
+++ src/app/books.ts
@@ -0,0 +1,6 @@
+export class Book {
+  isbn: string; //ISBN
+  title: string; //書名
+  price: number; //価格
+  publisher: string; //出版社
+}
